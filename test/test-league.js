47b0154bfed75ee641525eb6995a567b3ec21b36
Andres Rodriguez <andres@joyent.com>
2014-02-05 23:21:05 -0800

    Complete League endpoints

--- test/test-league.js
+++ test/test-league.js
@@ -8,6 +8,7 @@ var Client = require('../lib/client');
 var config = require('./config.json');
 var lol;
 var summonerId = 5908;
+var soloQ = 'RANKED_SOLO_5x5';
 
 exports.setUp = function (callback) {
 	lol = new Client(config);
@@ -15,8 +16,30 @@ exports.setUp = function (callback) {
 };
 
 
+exports.testLeagueChallenger = function(test) {
+	lol.leagueChallenger(soloQ, function (err, league) {
+		test.ifError(err);
+		test.ok(league);
+		test.ok(league.queue);
+		test.ok(league.name);
+		test.ok(league.entries);
+		test.done();
+	});
+};
+
+
 exports.testLeagueBySummoner = function(test) {
 	lol.leagueBySummoner(summonerId, function (err, leagues) {
+		test.ifError(err);
+		test.ok(leagues);
+		test.ok(leagues.length);
+		test.done();
+	});
+};
+
+
+exports.testLeagueBySummonerEntry = function(test) {
+	lol.leagueBySummonerEntry(summonerId, function (err, leagues) {
 		test.ifError(err);
 		test.ok(leagues);
 		test.ok(leagues.length);
